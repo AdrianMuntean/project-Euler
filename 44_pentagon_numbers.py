fc42bbe50f9c480c5a6159fa878416e6f700a36e
Adrian Muntean <m.adr.home@gmail.com>
2020-09-21 21:05:03 +0300

    Update 44_pentagon_numbers.py

--- 44_pentagon_numbers.py
+++ 44_pentagon_numbers.py
@@ -1,47 +1,48 @@
 import math
-
-sum = 0
+from itertools import islice
 
 pent = {}
 pent_values = {}
 
+
 def pentagonal(x):
     if pent.get(x):
         return pent[x]
-    
+
     p = int(x * (3 * x - 1) / 2)
     pent[x] = p
     pent_values[p] = x
     return p
 
+
 def is_pentagonal(x):
-    if pent_values.get(x):
-        return True
-    n_floor = math.floor((math.sqrt(3) * math.sqrt(x * 2 + 1 // 12) + 0.5) / 3)
-    n_round = math.ceil((math.sqrt(3) * math.sqrt(x * 2 + 1 // 12) + 0.5) / 3)
-    
-    return (n_floor * (3 * n_floor - 1) / 2 == x) or (n_round * (3 * n_round - 1) / 2 == x)
+    return pent_values.get(x)
 
 
-def try_find_petagonal_indexes(x):
-    for i in pent.keys():
-        value = pent_values.get(x - pent[i])
-        if value and value != i:
-            return (pent[i], pent[value])
+def find_all_petagonal_indexes(x):
+    pairs = []
+    for k, v in islice(pent.items(), len(pent) // 2):
+        value = pent_values.get(x - v)
+        if value and value != k:
+            pairs.append((v, pent[value]))
 
-    return (None, None)
+    return pairs
+
 
 def pentagon_numbers():
     d = 10**6
-    for i in range(1, 10**7):
+    for i in range(10**3, 10**5):
         pent_number_sum = pentagonal(i)
-        (a, b) = try_find_petagonal_indexes(pent_number_sum)
-        if a and b:
-            # print(f'{a} and {b} have the sum penta')
+        pairs = find_all_petagonal_indexes(pent_number_sum)
+        for p in pairs:
+            (a, b) = p
             diff = abs(a - b)
             if is_pentagonal(diff):
-                print(a, b)
-        
+                if d < diff:
+                    d = diff
+                    print(diff)
+
     return d
 
-print(pentagon_numbers())
+
+print(pentagon_numbers())  # 5482660
