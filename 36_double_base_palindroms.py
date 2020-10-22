223600cd2c1a82a335c7332d0ddc3514c7e45784
Adrian Muntean <m.adr.home@gmail.com>
2020-10-22 16:42:41 +0300

    Update 36_double_base_palindroms.py

--- 36_double_base_palindroms.py
+++ 36_double_base_palindroms.py
@@ -6,7 +6,7 @@ def binary(number):
     return [(number >> bit) & 1 for bit in range(num_bits - 1, -1, -1)]
 
 
-def is_palindrom(number_arr):
+def is_palindrome(number_arr):
     length = len(number_arr)
     for i in range(0, length):
         if number_arr[i] != number_arr[length - 1 - i]:
@@ -18,7 +18,7 @@ def is_palindrom(number_arr):
 def double_base_palindroms():
     sum = 0
     for number in range(1, 10 ** 6, 2):
-        if is_palindrom([int(x) for x in str(number)]) and is_palindrom(binary(number)):
+        if is_palindrome([int(x) for x in str(number)]) and is_palindrome(binary(number)):
             sum += number
 
     return sum
