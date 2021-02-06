4074d3d400fadb9cfc4589429d03050cb30a01f0
Adrian Muntean <m.adr.home@gmail.com>
2021-02-06 20:52:09 +0200

    Create 64_odd_period_square_roots.py

---
+++ 64_odd_period_square_roots.py
@@ -0,0 +1,29 @@
+# Need to revisit this problem, not really original code :|
+import math
+import itertools
+
+
+def is_square_root(r):
+    p = int(math.sqrt(r))
+    if p * p == r:
+        return True, None
+    return False, p
+
+
+def cf_period(r):
+    is_square, p = is_square_root(r)
+    if is_square:
+        return 0
+    q = 1
+    remainders = dict()
+
+    for pos in itertools.count(1):
+        q = (r-(p*p))/q
+        floor = int((math.sqrt(r)+p) / float(q))
+        p = -1 * (p - (floor*q))
+        if (p, q) in remainders:
+            return pos-remainders[p, q]
+        remainders[p, q] = pos
+
+
+print(len([x for x in range(2, 10001) if cf_period(x) % 2 == 1]))  # 1322
