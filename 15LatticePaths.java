1c709925366a37ecc7a84bc0325f94ff614b4dc2
Adrian Muntean <m.adr.home@gmail.com>
2021-10-21 08:18:03 +0300

    Update 15LatticePaths.java

--- 15LatticePaths.java
+++ 15LatticePaths.java
@@ -1,5 +1,10 @@
 import java.math.BigInteger;
 
+
+/*
+Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+*/
+
 public class LatticePaths {
     long noPaths = 0;
 
